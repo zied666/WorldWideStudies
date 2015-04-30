e7e4e37922a94c8ba7436b2da0179f99c906ad51
zied666 <zied.kharraz@gmail.com>
2015-04-30 09:58:35 +0100

    fix bug daashboard number admin

--- src/Back/GeneralBundle/Controller/DefaultController.php
+++ src/Back/GeneralBundle/Controller/DefaultController.php
@@ -13,7 +13,7 @@ class DefaultController extends Controller
         $SchoolsLocations=$em->getRepository("BackSchoolBundle:SchoolLocation")->findAll();
         $Accommodations=$em->getRepository("BackAccommodationBundle:Accommodation")->findAll();
         $Universitys=$em->getRepository("BackUniversityBundle:University")->findAll();
-        $admins=$this->findByRole("ROLE_ADMIN");
+        $admins=$this->findByRoleAdmin();
         return $this->render('BackGeneralBundle::dashboard.html.twig', array(
                     'countSchoolLocations'=>count($SchoolsLocations),
                     'countAccommodations'=>count($Accommodations),
@@ -22,13 +22,23 @@ class DefaultController extends Controller
         ));
     }
 
-    public function findByRole($role)
+    public function findByRoleAdmin()
     {
         $qb=$this->getDoctrine()->getManager()->createQueryBuilder();
         $qb->select('u')
                 ->from('BackUserBundle:User', 'u')
-                ->where('u.roles LIKE :roles')
-                ->setParameter('roles', '%"'.$role.'"%');
+                ->where('u.roles LIKE :role')
+                ->orWhere('u.roles LIKE :role1')
+                ->orWhere('u.roles LIKE :role2')
+                ->orWhere('u.roles LIKE :role3')
+                ->orWhere('u.roles LIKE :role4')
+                ->orWhere('u.roles LIKE :role5')
+                ->setParameter('role', '%ROLE_ADMIN%')
+                ->setParameter('role1', '%ROLE_SUPER_ADMIN%')
+                ->setParameter('role2', '%ROLE_ADMIN_UNIVERSITY%')
+                ->setParameter('role3', '%ROLE_ADMIN_ACCOMMODATION%')
+                ->setParameter('role4', '%ROLE_ADMIN_SCHOOL%')
+                ->setParameter('role5', '%ROLE_ADMIN_CONFIG%');
         return $qb->getQuery()->getResult();
     }
 
