e58962aa6582e9618caefc59e87f81a245fd3834
zied666 <zied.kharraz@gmail.com>
2015-05-05 16:55:25 +0100

    add front end template

--- src/Front/GeneralBundle/Controller/DefaultController.php
+++ src/Front/GeneralBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('FrontGeneralBundle::accueil.html.twig');
+        return $this->render('FrontGeneralBundle::accueil2.html.twig');
     }
 }
