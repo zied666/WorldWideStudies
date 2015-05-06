7f7c1e13181e854ea9e5060cdbd6f0b3ac81ccff
zied666 <zied.kharraz@gmail.com>
2015-05-06 08:31:13 +0100

    push two homepages

--- src/Front/GeneralBundle/Controller/DefaultController.php
+++ src/Front/GeneralBundle/Controller/DefaultController.php
@@ -8,6 +8,18 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
+        return $this->render('FrontGeneralBundle::accueil.html.twig');
+    }
+    public function index1Action()
+    {
+        return $this->render('FrontGeneralBundle::accueil1.html.twig');
+    }
+    public function index2Action()
+    {
         return $this->render('FrontGeneralBundle::accueil2.html.twig');
+    }
+    public function index3Action()
+    {
+        return $this->render('FrontGeneralBundle::accueil3.html.twig');
     }
 }
