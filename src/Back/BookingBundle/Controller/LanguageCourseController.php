4858d0315edaf9974ab4c0261fc8ca85102633b1
zied666 <zied.kharraz@gmail.com>
2015-06-04 09:35:33 +0100

    add popup to language course

--- src/Back/BookingBundle/Controller/LanguageCourseController.php
+++ src/Back/BookingBundle/Controller/LanguageCourseController.php
@@ -4,6 +4,7 @@ namespace Back\BookingBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class LanguageCourseController extends Controller
 {
@@ -19,6 +20,87 @@ class LanguageCourseController extends Controller
         return $this->render('BackBookingBundle:LanguageCourse:list.html.twig', array(
                     'bookings'=>$bookings
         ));
+    }
+
+    public function ajaxPaypalAction()
+    {
+        $em=$this->getDoctrine()->getManager();
+        $request=$this->getRequest();
+        $id=$request->get("id");
+        $tab=array();
+        $response=new JsonResponse();
+        $booking=$em->getRepository("FrontGeneralBundle:BookingLanguageCourse")->find($request->get('id'));
+        $tab['title']='Paypal information';
+        $tab['data']=$booking->getPaypalData();
+        $response->setData($tab);
+        return $response;
+    }
+
+    public function ajaxCourseAction()
+    {
+        $em=$this->getDoctrine()->getManager();
+        $request=$this->getRequest();
+        $id=$request->get("id");
+        $tab=array();
+        $tab=array();
+        $response=new JsonResponse();
+        $booking=new \Front\GeneralBundle\Entity\BookingLanguageCourse();
+        $booking=$em->getRepository("FrontGeneralBundle:BookingLanguageCourse")->find($request->get('id'));
+        $tab['title']=$booking->getCourse()->getName();
+        $tab['data']=array();
+        $tab['data']['Course']=$booking->getCourse()->getName();
+        $tab['data']['Duration']=$booking->getWeekCourse().' weeks';
+        $tab['data']['Starting date']=$booking->getStartingDateCourse()->format('l d/m/Y');
+        $tab['data']['Price']=$booking->getTotalCourse().' '.$booking->getCurrency()->getCode();
+        $response->setData($tab);
+        return $response;
+    }
+
+    public function ajaxAccommodationAction()
+    {
+        $em=$this->getDoctrine()->getManager();
+        $request=$this->getRequest();
+        $id=$request->get("id");
+        $tab=array();
+        $tab=array();
+        $response=new JsonResponse();
+        $booking=new \Front\GeneralBundle\Entity\BookingLanguageCourse();
+        $booking=$em->getRepository("FrontGeneralBundle:BookingLanguageCourse")->find($request->get('id'));
+        if(!is_null($booking->getRoom()))
+        {
+            $tab['title']=$booking->getRoom()->getAccommodation()->getName();
+            $tab['data']=array();
+            $tab['data']['Accommodation']=$booking->getRoom()->getAccommodation()->getName();
+            $tab['data']['room']=$booking->getRoom()->getName();
+            $tab['data']['Duration']=$booking->getWeekAccommodation().' weeks';
+            $tab['data']['Starting date']=$booking->getStartingDateAccommodation()->format('l d/m/Y');
+            $tab['data']['Price']=$booking->getTotalAccommodation().' '.$booking->getCurrency()->getCode();
+            $response->setData($tab);
+        }
+        return $response;
+    }
+
+    public function ajaxExtrasAction()
+    {
+        $em=$this->getDoctrine()->getManager();
+        $request=$this->getRequest();
+        $id=$request->get("id");
+        $tab=array();
+        $tab=array();
+        $response=new JsonResponse();
+        $booking=new \Front\GeneralBundle\Entity\BookingLanguageCourse();
+        $booking=$em->getRepository("FrontGeneralBundle:BookingLanguageCourse")->find($request->get('id'));
+        $tab['title']='Extras : '.count($booking->getExtras());
+        $tab['data']=array();
+        $i=0;
+        foreach($booking->getExtras() as $extra)
+        {
+            $i++;
+            $tab['data']['Extra : '.$i]=$extra->getName();
+        }
+       
+        $response->setData($tab);
+        return $response;
     }
 
 }
